e7d1ecd96f41d431e06ad0f8e11413542045ee1c
Jayesh Sharma <jayeshsh1707@gmail.com>
2023-05-07 13:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function threeSum(arr, target) {
     arr.sort((a, b) => a - b);
     let closestSum = infinity;
-}
+
 for (let i = 0; i < arr.length - 2; i++) {
     let left = i + 1;
     let right = arr.length - 1;
